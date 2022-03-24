fa2b36ecea9b11c17cf8ef3b74820068820cb38e
Ian Hornik <yiliansource@gmail.com>
2022-03-24 16:22:24 +0100

    fixed ios chrome spacing bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,15 @@ import { Box } from "@mui/system";
 import React from "react";
 
 import { GroupApp } from "src/components/app/GroupApp";
+import { useClientOnly } from "src/lib/hooks/useClientOnly";
 import { isIosChrome } from "src/lib/supports";
 
 export default function Index() {
+    const isClient = useClientOnly();
+
     return (
         <>
-            {isIosChrome() && (
+            {isClient && isIosChrome() && (
                 <Box mb={2}>
                     <Alert severity="error">
                         This application cannot download files in iOS Chrome. Please use a different browser.
